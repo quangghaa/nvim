6e71432bc6aa07f18ccf24ac6220dc60cad2cb90
harry_nguyen <ext.harry.nguyen@inseinc.com>
2025-03-24 14:10:59 +0700

    theme ivy telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,10 +2,18 @@ return {
 	"nvim-telescope/telescope.nvim",
 
 	config = function()
-		require("telescope").setup({})
+		require("telescope").setup({
+			pickers = {
+				find_files = {
+					theme = "ivy",
+				},
+				live_grep = {
+					theme = "ivy",
+				},
+			},
+		})
 
 		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
 		vim.keymap.set("n", "<C-p>", builtin.git_files, {})
 		vim.keymap.set("n", "<leader>pws", function()
 			local word = vim.fn.expand("<cword>")
@@ -18,6 +26,14 @@ return {
 		vim.keymap.set("n", "<leader>ps", function()
 			builtin.grep_string({ search = vim.fn.input("Grep > ") })
 		end)
-		vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
+		vim.keymap.set("n", "<space>en", function()
+			require("telescope.builtin").find_files({
+				cwd = vim.fn.stdpath("config"),
+			})
+		end)
+		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
 	end,
 }
