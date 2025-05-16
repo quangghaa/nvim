4c4da658ce3d152d7af144b3dbc78a2142e19e8e
hnguyen <ext.harry.nguyen@inseinc.com>
2025-05-16 18:52:16 +0700

    add avante

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -78,7 +78,7 @@ return {
 					vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
 					vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
 					vim.keymap.set({ "n", "x" }, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
-					vim.keymap.set("n", "<leader>vca", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+					vim.keymap.set("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
 				end,
 			})
 
